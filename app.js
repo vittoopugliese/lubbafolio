173c2949758b336b3beac33643caeb4819bdf26d
Vittorio <vittoopugliese@gmail.com>
2022-10-05 19:16:09 -0300

    final por hoy jaja

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const imagesContainer = document.querySelector(".imagesContainer");
 
-for (let i = 1; i < 13; i++) {
-  imagesContainer.innerHTML += `<img class="image" src="assets/m (${i}).png" draggable=false alt="lubba image">`;
+for (let i = 0; i < 12; i++) {
+  imagesContainer.innerHTML += `<img class="image" src="assets/m (${i + 1}).png" draggable=false alt="lubba image">`;
 }
 
+const html = document.querySelector("html");
+const modal = document.querySelector(".modal");
+const modalBg = document.querySelector(".modalBg");
+const imageModal = document.querySelector(".imageModal");
+
 window.addEventListener("DOMContentLoaded", () => {
-  const image = document.querySelectorAll(".image");
-  for (let i = 1; i < 12; i++) {
-    let images = image[i]
-    images.addEventListener("click", () => {
-      image.style.width = "10px";
-      console.log("asdasd");
+  const images = document.querySelectorAll(".image");
+  for (let i = 0; i < 12; i++) {
+    images[i].addEventListener("click", () => {
+      imageModal.innerHTML = `<img class="modalImage" draggable=false src="${images[i].getAttribute("src")}" >`;
+      modal.style.display = "flex";
+      modalBg.style.display = "flex";
+      setTimeout(() => {
+        modal.style.opacity = "1";
+        modalBg.style.opacity = "1";
+        html.style.overflow = "hidden";
+      }, 154);
     });
   }
 });
+
+modalBg.addEventListener("click", closeModal);
+function closeModal() {
+  modal.style.opacity = "0";
+  modalBg.style.opacity = "0";
+  setTimeout(() => {
+    modal.style.display = "none";
+    modalBg.style.display = "none";
+    html.style.overflow = "auto";
+  }, 200);
+}
