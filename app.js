87e14f62748f80faa12c8c2cfb7549ced50a47c7
Vittorio <vittoopugliese@gmail.com>
2022-10-05 18:13:42 -0300

    first

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const imagesContainer = document.querySelector(".imagesContainer");
 
 for (let i = 1; i < 12; i++) {
   imagesContainer.innerHTML += `<img class="image" src="assets/m (${i}).png" draggable=false alt="lubba image">`;
-
 }
 
-const images = document.querySelectorAll(".image");
-images.addEventListener("click", () => {
-  image.style.width = "10px";
-  console.log("asdasd");
+window.addEventListener("DOMContentLoaded", () => {
+  const image = document.querySelectorAll(".image");
+  for (let i = 1; i < 12; i++) {
+    let images = image[i]
+    images.addEventListener("click", () => {
+      image.style.width = "10px";
+      console.log("asdasd");
+    });
+  }
 });
